24fa72c54056a195222e620abd90ed540edfff0c
Ayush Roy <ayush30085@gmail.com>
2025-04-21 00:31:04 +0530

    add user middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,9 @@ import { z } from "zod";
 import bcrypt from "bcrypt";
 import { userModel } from "./db";
 import dbConnection from "./db-server";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { userMiddleware } from "./middleware";
 const port = 3000;
 const app = express();
 app.use(express.json());
@@ -31,11 +34,12 @@ app.post("/api/v1/signup", async (req, res) => {
     return;
   }
 
-  const { username, password } = req.body;
+  const { username, password, email } = req.body;
 
   const hashedpassword = await bcrypt.hash(password, 5);
 
   try {
+    // also need to store email
     await userModel.create({
       username,
       password: hashedpassword,
@@ -50,8 +54,37 @@ app.post("/api/v1/signup", async (req, res) => {
   });
 });
 
-app.post("/api/v1/signin", (req, res) => {});
-app.post("/api/v1/content", (req, res) => {});
+app.post("/api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  const findUser = await userModel.findOne({
+    username,
+  });
+  if (findUser && findUser.password) {
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+    if (passwordMatch) {
+      const token = jwt.sign(
+        {
+          id: findUser._id.toString(),
+        },
+        JWT_SECRET
+      );
+      res.status(403).json({
+        token,
+      });
+    } else {
+      res.json({
+        message: "incorrect password",
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: "user not signed up",
+    });
+  }
+});
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {});
 app.get("/ap/v1i/content", (req, res) => {});
 app.delete("/api/v1/content", (req, res) => {});
 app.post("/api/v1/sharelink", (req, res) => {});
